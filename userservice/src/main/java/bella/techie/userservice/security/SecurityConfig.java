b9acb5c8096e7e4070b49de1eb6d52ac0c88974c
Adeniyi-Bella <adeniyibella@gmail.com>
2023-09-07 15:32:19 +0200

    feat(authentication): Implemented authentication filter and password encrypting

---
+++ userservice/src/main/java/bella/techie/userservice/security/SecurityConfig.java
@@ -0,0 +1,53 @@
+package bella.techie.userservice.security;
+
+import bella.techie.userservice.filter.CustomAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.security.config.http.SessionCreationPolicy.*;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    //implemented in service implementation
+    private final UserDetailsService userDetailsService;
+    //implemented in main class with a bean
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(STATELESS);
+        //allow everyone to be able to access requests at this point
+        /* http.authorizeRequests().anyRequest().permitAll();*/
+        http.authorizeRequests().antMatchers("/login").permitAll();
+        http.authorizeRequests().antMatchers(GET, "/api/user/**").hasAnyAuthority("ROLE_USER");
+        http.authorizeRequests().antMatchers(POST, "/api/user/save/**").hasAnyAuthority("ROLE_ADMIN");
+         http.authorizeRequests().anyRequest().authenticated();
+        http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        //super to refer to the parent class
+        return super.authenticationManagerBean();
+    }
+
+}
--- userservice/src/main/java/bella/techie/userservice/service/UserServiceImpl.java
+++ userservice/src/main/java/bella/techie/userservice/service/UserServiceImpl.java
@@ -5,23 +5,51 @@ import bella.techie.userservice.domain.Role;
 import bella.techie.userservice.domain.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import bella.techie.userservice.User.UserRepo;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 @Slf4j
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService, UserDetailsService {
     private final RoleRepo roleRepo;
     private final UserRepo userRepo;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByUsername(username);
+        if (user == null) {
+            log.error("User not found");
+            throw new UsernameNotFoundException("User not found in DB");
+        } else {log.info("User found in the DB: {}", username); }
+
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        //get current user and create a granted authority based on the role
+        user.getRoles().forEach(role -> {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        });
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                authorities);
+    }
     @Override
     public User saveUser(User user) {
         log.info("Saving new user {} to the DB", user.getName());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepo.save(user);
     }
 
@@ -54,4 +82,6 @@ public class UserServiceImpl implements UserService{
         log.info("Fetching all users");
         return userRepo.findAll();
     }
+
+
 }
